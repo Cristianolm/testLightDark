f1d1a86484d8ed21688b9171419ba640187ce520
Cristiano <cristianomartins96@hotmail.com>
2024-05-02 00:05:23 +0100

    Deploying site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-zinc-100 dark:bg-zinc-800 flex items-center justify-center transition-colors duration-300">
+        <label className={`relative flex cursor-pointer items-center `}>
+          <input id="themeCheckbox" type="checkbox" className="peer sr-only" />
+          <div className="peer flex h-8 w-12 items-center rounded-full  bg-zinc-300  object-scale-down px-1 shadow-inner after:relative  after:left-0 after:flex after:h-6 after:w-6 after:rotate-0 after:animate-moon after:rounded-full after:bg-zinc-100 after:bg-[url('/img/moon.svg')] after:bg-center after:bg-no-repeat after:transition-all after:content-[''] peer-checked:after:left-4 after:peer-checked:rotate-180 after:peer-checked:animate-sun peer-checked:after:bg-[url('/img/sun.svg')] dark:bg-zinc-700 dark:after:bg-zinc-800 sm:h-8 sm:w-14 sm:after:h-7 sm:after:w-7 md:h-10 md:w-20 md:after:h-9 md:after:w-9 md:peer-checked:after:left-9"></div>
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default App;
